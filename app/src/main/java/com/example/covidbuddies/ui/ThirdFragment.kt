3b0904622a1192844fc474ec235374cd9d8dee6d
Rhaelia <gwenkiler@gmail.com>
2020-11-07 14:22:30 -0800

    Set up three fragments with icons. editable through the respective XML files.

---
+++ app/src/main/java/com/example/covidbuddies/ui/ThirdFragment.kt
@@ -0,0 +1,18 @@
+package com.example.covidbuddies.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.covidbuddies.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ThirdFragment : Fragment(R.layout.fragment_third)
+{
+    
+}
